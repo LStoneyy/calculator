112a184411eae68bac2d3033110d394bf8fdbc43
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2024-05-21 21:05:59 +0200

    Created basic operate function

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-const add = function(a, b) {
+let var1;
+let var2;
+let operator;
+
+function add(a, b) {
 	return a+b;
 };
 
-const subtract = function(a, b) {
+function subtract(a, b) {
 	return a - b;
 };
 
-const multiply = function(a, b) {
+function multiply(a, b) {
     return a*b;
 };
 
-const divide = function(a, b) {
+function divide(a, b) {
     return a/b;
 };
+
+function operate(var1, var2, operator) {
+ if (operator === "+") {
+    return add(var1, var2);
+ } else if (operator === "-") {
+    return subtract(var1, var2);
+ } else if (operator === "*") {
+    return multiply(var1, var2);
+ } else if (operator === "/") {
+    return divide(var1, var2);
+ }
+}
+
+console.log(operate(2, 3, "-"))
